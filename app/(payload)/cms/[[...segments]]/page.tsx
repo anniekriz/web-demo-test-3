740f3dd638ef5f2497df7d5f451621936ad192d4
ANNIE\annau <anna.ukrizu@gmail.com>
2026-02-21 10:54:51 +0100

    Fix next.config.ts

--- app/(payload)/cms/[[...segments]]/page.tsx
+++ app/(payload)/cms/[[...segments]]/page.tsx
@@ -1,5 +1,20 @@
+import type { Metadata } from 'next'
 import config from '@payload-config'
 import { generatePageMetadata, RootPage } from '@payloadcms/next/views'
 
-export default RootPage
-export const generateMetadata = generatePageMetadata({ config })
+type PageProps = {
+  params: { segments?: string[] }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export const generateMetadata = async (
+  { params, searchParams }: PageProps,
+): Promise<Metadata> => {
+  return generatePageMetadata({ config, params, searchParams })
+}
+
+const Page = ({ params, searchParams }: PageProps) => {
+  return RootPage({ config, params, searchParams })
+}
+
+export default Page
